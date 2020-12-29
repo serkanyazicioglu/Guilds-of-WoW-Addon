2974970e98cee3dc01196ab30d9f87ec50a6f676
Serkan Yazıcıoğlu <serkanyazicioglu@gmail.com>
2020-12-30 01:34:13 +0300

    Bug fixes and ui changes

--- Core.lua
+++ Core.lua
@@ -15,6 +15,7 @@ f:RegisterEvent("CALENDAR_CLOSE_EVENT")
 local isProcessing = false
 local isPropogatingUpdate = false
 local eventContainer = {}
+local eventContainerScroll = {}
 
 function GOW:OnInitialize()
 	self.GUI = LibStub("AceGUI-3.0")
@@ -34,14 +35,34 @@ function GOW:OnInitialize()
 	end
 
 	eventContainer = GOW.GUI:Create("Frame")
-	eventContainer:SetLayout("List")
+	eventContainer:SetLayout("Fill")
+	eventContainer:SetHeight(550)
 	eventContainer:SetTitle("Guilds of WoW Event Creation")
 	eventContainer:SetStatusText("Guilds of WoW. Type /gow for quick access")
 	eventContainer:SetCallback("OnClose", function(widget) eventContainer:Hide() end)
-	--eventContainer:SetCallback("OnEscapePressed", function(widget) eventContainer:Hide() end)
+	eventContainer:SetCallback("OnEscapePressed", function(widget) eventContainer:Hide() end)
 	--tinsert(UISpecialFrames, eventContainer:GetName())
 	
 	eventContainer:Hide()
+
+	eventContainerScroll = GOW.GUI:Create("ScrollFrame")
+	eventContainerScroll:SetLayout("Flow")
+	eventContainerScroll:SetFullWidth(true)
+	eventContainerScroll:SetFullHeight(true)
+	eventContainer:AddChild(eventContainerScroll)
+
+	StaticPopupDialogs["CONFIRM_EVENT_CREATION"] = {
+		text = "Are you sure you want to create this event?",
+		button1 = "Yes",
+		button2 = "No",
+		OnAccept = function()
+			C_Calendar.AddEvent()
+		end,
+		timeout = 0,
+		whileDead = true,
+		hideOnEscape = true,
+		preferredIndex = 3,
+	  }
 end
 
 f:SetScript("OnEvent", function(self,event, ...)
@@ -76,22 +97,7 @@ function Core:CreateUpcomingEvents()
 		return
 	end
 
-	StaticPopupDialogs["EXAMPLE_HELLOWORLD"] = {
-		text = "Do you want to greet the world today?",
-		button1 = "Yes",
-		button2 = "No",
-		OnAccept = function()
-			--GreetTheWorld()
-		end,
-		timeout = 0,
-		whileDead = true,
-		hideOnEscape = true,
-		preferredIndex = 3,  -- avoid some UI taint, see http://www.wowace.com/announcements/how-to-avoid-some-ui-taint/
-	  }
-
-	  StaticPopup_Show ("EXAMPLE_HELLOWORLD")
-
-	eventContainer:ReleaseChildren()
+	eventContainerScroll:ReleaseChildren()
 
 	local guildName, _, _, realmName = GetGuildInfo("player")
 
@@ -129,6 +135,8 @@ function Core:CreateUpcomingEvents()
 				print("Event belongs to another guild")
 			end
 		end
+
+		eventContainerScroll:DoLayout()
 	end
 
 	isPropogatingUpdate = false
@@ -151,20 +159,6 @@ function Core:searchForEvent(event)
 				if (string.match(dayEvent.title, "*" .. event.eventKey)) then
 					return i
 				end
-
-				-- C_Calendar.OpenEvent(0, event.day, i)
-				-- -- local otherEvent = C_Calendar.GetEventInfo()
-
-				-- if (otherEvent ~= nil) then
-				-- 	print("description: " .. otherEvent.description)
-				-- else 
-				-- 	print("event is null")
-				-- end
-
-				-- if (otherEvent ~= nil and string.match(otherEvent.description, "*" .. event.eventKey)) then
-				-- 	C_Calendar.CloseEvent()
-				-- 	return i
-				-- end
 			end
 		end
 	end
@@ -178,24 +172,56 @@ function Core:AppendCalendarList(event)
 
 	local eventGroup = GOW.GUI:Create("InlineGroup")
 	eventGroup:SetTitle(event.title)
+	eventGroup:SetFullWidth(true)
 
-	local eventHeading = GOW.GUI:Create("InteractiveLabel")
-	eventHeading:SetText(event.title)
-	eventHeading:SetFont(fontPath, 15)
-	eventHeading:SetFullWidth(true)
-	--eventGroup:AddChild(eventHeading)
+	local eventDescriptionLabel = GOW.GUI:Create("SFX-Info")
+	eventDescriptionLabel:SetLabel("Description")
+	eventDescriptionLabel:SetText(event.description)
+	eventGroup:AddChild(eventDescriptionLabel)
 
-	local eventTitleLabel = GOW.GUI:Create("Label")
-	eventTitleLabel:SetText(event.description)
-	eventTitleLabel:SetFont(fontPath, fontSize)
-	eventHeading:SetFullWidth(true)
-	eventGroup:AddChild(eventTitleLabel)
-
-	local eventDateLabel = GOW.GUI:Create("Label")
+	local eventDateLabel = GOW.GUI:Create("SFX-Info")
+	eventDateLabel:SetLabel("Date")
 	eventDateLabel:SetText(event.dateText)
-	eventDateLabel:SetFont(fontPath, fontSize)
 	eventGroup:AddChild(eventDateLabel)
+
+	local eventHourLabel = GOW.GUI:Create("SFX-Info")
+	eventHourLabel:SetLabel("Hour")
+	eventHourLabel:SetText(event.hourText)
+	eventGroup:AddChild(eventHourLabel)
+
+	local eventDurationLabel = GOW.GUI:Create("SFX-Info")
+	eventDurationLabel:SetLabel("Duration")
+	eventDurationLabel:SetText(event.durationText)
+	eventGroup:AddChild(eventDurationLabel)
 	
+	local levelText = event.minLevel
+
+	if event.minLevel ~= event.maxLevel then
+		levelText = levelText .. " -> " .. event.maxLevel
+	end
+
+	local eventLevelLabel = GOW.GUI:Create("SFX-Info")
+	eventLevelLabel:SetLabel("Level")
+	eventLevelLabel:SetText(levelText)
+	eventGroup:AddChild(eventLevelLabel)
+
+	if (event.minItemLevel > 0) then
+		local eventMinItemLevelLabel = GOW.GUI:Create("SFX-Info")
+		eventMinItemLevelLabel:SetLabel("Item Level")
+		eventMinItemLevelLabel:SetText(event.minItemLevel .. "+")
+		eventGroup:AddChild(eventMinItemLevelLabel)
+	end
+	
+	local eventInvitingMembersLabel = GOW.GUI:Create("SFX-Info")
+	eventInvitingMembersLabel:SetLabel("Inviting")
+
+	if (event.isGuildEvent and event.minItemLevel == 0) then
+		eventInvitingMembersLabel:SetText("All guildies within level range")
+	else 
+		eventInvitingMembersLabel:SetText(event.totalMembers .. " members")
+	end
+	eventGroup:AddChild(eventInvitingMembersLabel)
+
 	local eventIndex = Core:searchForEvent(event)
 
 	local button = GOW.GUI:Create("Button")
@@ -213,9 +239,8 @@ function Core:AppendCalendarList(event)
 	end
 	eventGroup:AddChild(button)
 
-	eventContainer:AddChild(eventGroup)
+	eventContainerScroll:AddChild(eventGroup)
 end
-
 
 function Core:CreateCalendarEvent(event)
 	local eventIndex = Core:searchForEvent(event)
@@ -229,15 +254,8 @@ function Core:CreateCalendarEvent(event)
 			print("Event passed: " .. event.id)
 		else
 			print("Creating event: ".. event.id .. " : " .. event.title .. " : " .. event.eventKey)
-			--local test = GetGuildInfo("player")
-			--if (test == nil)
-			-- then
-				--   status = false 
-			--end
-
 			C_Calendar.CloseEvent()
 			C_Calendar.CreatePlayerEvent()
-			--end
 			C_Calendar.EventSetTitle(event.titleWithKey)
 			C_Calendar.EventSetDescription(event.description)
 			C_Calendar.EventSetType(event.eventType)
@@ -251,85 +269,8 @@ function Core:CreateCalendarEvent(event)
 					C_Calendar.EventInvite(event.inviteMembers[i].characterName)
 				end
 			end
-			Core:ShowConfirmWindow(event)
+			
+			StaticPopup_Show ("CONFIRM_EVENT_CREATION")
 		end
 	end
-end
-
-function Core:ShowConfirmWindow(event)
-	local frame = GOW.GUI:Create("Window")
-	--frame:SetCallback("OnClose",function(widget) C_Calendar.AddEvent(); RCE.core:scheduleRepeatCheck(); frame:Release() end)
-	--frame:SetCallback("OnClose",function(widget) RCE.core:scheduleRepeatCheck() end)
-	frame:SetLayout("List")
-	frame:EnableResize(false)
-	frame:SetTitle("Guilds of WoW Event Creation")
-	frame:SetWidth(400)
-	frame:SetHeight(400)
-	frame:SetFullHeight(false)
-	frame:SetCallback("OnClose", function(widget) GOW.GUI:Release(widget) eventContainer:Show() end)
-
-	local fontPath = "Fonts\\FRIZQT__.TTF"
-	local fontSize = 12
-
-	local confirmLabel = GOW.GUI:Create("Label")
-	confirmLabel:SetText("A new event is about to be created!")
-	confirmLabel:SetFont(fontPath, fontSize)
-	frame:AddChild(confirmLabel)
-
-	local eventTitleLabel = GOW.GUI:Create("Label")
-	eventTitleLabel:SetText("Event Name: " .. event.title)
-	eventTitleLabel:SetFont(fontPath, fontSize)
-	--eventTitleLabel:SetWordWrap(true)
-	frame:AddChild(eventTitleLabel)
-
-	local eventDescriptionLabel = GOW.GUI:Create("Label")
-	eventDescriptionLabel:SetText("Event Description: " .. event.description)
-	eventDescriptionLabel:SetFont(fontPath, fontSize)
-	frame:AddChild(eventDescriptionLabel)
-
-	local eventDateLabel = GOW.GUI:Create("Label")
-	eventDateLabel:SetText("Date: " .. event.dateText)
-	eventDateLabel:SetFont(fontPath, fontSize)
-	frame:AddChild(eventDateLabel)
-
-	local eventMinLevelLabel = GOW.GUI:Create("Label")
-	eventMinLevelLabel:SetText("Min Level: " .. event.minLevel)
-	eventMinLevelLabel:SetFont(fontPath, fontSize)
-	frame:AddChild(eventMinLevelLabel)
-
-	local eventMaxLevelLabel = GOW.GUI:Create("Label")
-	eventMaxLevelLabel:SetText("Max Level: " .. event.maxLevel)
-	eventMaxLevelLabel:SetFont(fontPath, fontSize)
-	frame:AddChild(eventMaxLevelLabel)
-
-	if (event.minItemLevel > 0) then
-		local eventMinItemLevelLabel = GOW.GUI:Create("Label")
-		eventMinItemLevelLabel:SetText("Min Item Level: " .. event.minItemLevel)
-		eventMinItemLevelLabel:SetFont(fontPath, fontSize)
-		frame:AddChild(eventMinItemLevelLabel)
-	end
-	
-	local eventInvitingMembersLabel = GOW.GUI:Create("Label")
-	eventInvitingMembersLabel:SetFont(fontPath, fontSize)
-
-	if (event.isGuildEvent and event.minItemLevel == 0) then
-		eventInvitingMembersLabel:SetText("Inviting Members: All guildies within level range")
-	else 
-		eventInvitingMembersLabel:SetText("Inviting Members: " .. event.totalMembers)
-	end
-
-	frame:AddChild(eventInvitingMembersLabel)
-
-	local button = GOW.GUI:Create("Button")
-	button:SetText("Confirm")
-	button:SetCallback("OnClick", function()
-		--beforeAddFunc()
-		C_Calendar.AddEvent()
-		frame:Release()
-	end)
-	frame:AddChild(button)
-	eventContainer:Hide()
-	GOW.GUI:SetFocus(frame)
-
-	--PlaySound(SOUNDKIT.READY_CHECK)
 end
---
+++ libs/AceGUI-3.0_SFX-Widgets/CHANGELOG.md
@@ -0,0 +1,5 @@
+## 1.0.5
+
+- Added `itIT` translations. (faniel80)
+- Update `zhTW` translations. (RainbowUI)
+- Updated the `Interface` version for _Retail_ to `90002`.
